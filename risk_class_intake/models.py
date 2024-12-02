71bb04b92376387388875979c9a9d35662785ef2
EAM26 <emuller2607@gmail.com>
2024-12-02 21:14:30 +0100

    Add middle part of form to index html with storage in database

--- risk_class_intake/models.py
+++ risk_class_intake/models.py
@@ -2,14 +2,26 @@ from django.db import models
 
 
 class Form(models.Model):
-    name = models.CharField(max_length=80)
-    address = models.CharField(max_length=80)
-    zip_code = models.CharField(max_length=80)
-    city = models.CharField(max_length=80)
-    requesting_party = models.CharField(max_length=80)
-    risk_class = models.CharField(max_length=80)
+    name = models.CharField(max_length=80, null=True, blank=True)
+    address = models.CharField(max_length=80, null=True, blank=True)
+    zip_code = models.CharField(max_length=80, null=True, blank=True)
+    city = models.CharField(max_length=80, null=True, blank=True)
+    requesting_party = models.CharField(max_length=80, null=True, blank=True)
+    risk_class = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateField()
-    email = models.EmailField()
+    email = models.EmailField(max_length=80, null=True, blank=True)
+    organizational = models.CharField(max_length=80, null=True, blank=True)
+    constructional = models.CharField(max_length=80, null=True, blank=True)
+    compartment = models.CharField(max_length=80, null=True, blank=True)
+    carryaway = models.CharField(max_length=80, null=True, blank=True)
+    electronic = models.CharField(max_length=80, null=True, blank=True)
+    shield = models.CharField(max_length=80, null=True, blank=True)
+    alarm = models.CharField(max_length=80, null=True, blank=True)
+    verification = models.CharField(max_length=80, null=True, blank=True)
+    reaction = models.CharField(max_length=80, null=True, blank=True)
+    customization = models.CharField(max_length=80, null=True, blank=True)
+    partial_security = models.CharField(max_length=80, null=True, blank=True)
+    maintenance = models.CharField(max_length=80, null=True, blank=True)
 
     def __str__(self):
         return f"{self.name}\n {self.city}"
--- risk_class_intake/views.py
+++ risk_class_intake/views.py
@@ -16,6 +16,18 @@ def index(request):
             risk_class = form.cleaned_data["risk_class"]
             date = datetime.now().date()  # Get the current date
             email = form.cleaned_data["email"]
+            organizational = form.cleaned_data["organizational"]
+            constructional = form.cleaned_data["constructional"]
+            compartment = form.cleaned_data["compartment"]
+            carryaway = form.cleaned_data["carryaway"]
+            electronic = form.cleaned_data["electronic"]
+            shield = form.cleaned_data["shield"]
+            alarm = form.cleaned_data["alarm"]
+            verification = form.cleaned_data["verification"]
+            reaction = form.cleaned_data["reaction"]
+            customization = form.cleaned_data["customization"]
+            partial_security = form.cleaned_data["partial_security"]
+            maintenance = form.cleaned_data["maintenance"]
 
             # Debugging: Print the cleaned data
             print(f"Name: {name}, Address: {address}, Zip Code: {zip_code},"
@@ -25,7 +37,14 @@ def index(request):
             # Save the form data to the database
             Form.objects.create(name=name, address=address, zip_code=zip_code,
                                 city=city, requesting_party=requesting_party,
-                                risk_class=risk_class, date=date, email=email)
+                                risk_class=risk_class, date=date, email=email,
+                                organizational=organizational, constructional=
+                                constructional, compartment=compartment,
+                                carryaway=carryaway, electronic=electronic,
+                                shield=shield, alarm=alarm, verification=
+                                verification, reaction=reaction,
+                                customization=customization, partial_security=
+                                partial_security, maintenance=maintenance)
             return redirect('index')
         else:
             # Debugging: Print form errors
